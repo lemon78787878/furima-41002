061445dc89afb66891b2a314848950afb328a86c
lemon78787878 <wakuwaku.5611@gmail.com>
2024-06-26 18:55:34 +0900

    新規登録/ログイン/ログアウトの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,27 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: true
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :rb_last_name, presence: true
-  validates :rb_first_name, presence: true
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'は全角文字を使用してください' } do
+    validates :nickname
+    validates :first_name
+    validates :last_name
+  end
+
+  with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "はカタカナで入力してください" } do
+    validates :rb_last_name
+    validates :rb_first_name
+  end
+
   validates :birthday, presence: true
+
+  validate :password_complexity
+
+  private
+
+  def password_complexity
+    return if password.nil? || password.match?(/\A(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]+\z/)
+
+    errors.add :password, 'は半角英数字混合で入力してください'
+  end
 
 end
