68905d98014448fce0c120c6a56d74a6bf36247e
lemon78787878 <wakuwaku.5611@gmail.com>
2024-06-26 14:52:27 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  validates :last_name, presence: true
+  validates :first_name, presence: true
+  validates :rb_last_name, presence: true
+  validates :rb_first_name, presence: true
+  validates :birthday, presence: true
+
 end
