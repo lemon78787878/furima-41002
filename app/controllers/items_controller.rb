4e81e144cf94ce38a47045d6bf6ada9b25d184c7
lemon78787878 <wakuwaku.5611@gmail.com>
2024-06-28 15:38:36 +0900

    ActiveStorageの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,4 +3,17 @@ class ItemsController < ApplicationController
   def index
   end
 
+  def new
+    @tweet = Tweet.new
+  end
+
+  def create
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:item, :description, :category_id, :condition_id, :post_cost_id, :sender_id, :ship_date_id, :price, :image).merge(user_id: current_user.id)
+  end
+
 end
