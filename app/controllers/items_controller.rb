94efc508eff3d3c469ed4a95760628d5a639b3d7
lemon78787878 <wakuwaku.5611@gmail.com>
2024-06-28 16:28:50 +0900

    ActiveHashを導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,7 +4,6 @@ class ItemsController < ApplicationController
   end
 
   def new
-    @tweet = Tweet.new
   end
 
   def create
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,14 +1,24 @@
 class Item < ApplicationRecord
+  
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :post_cost
+  belongs_to :sender
+  belongs_to :ship_date
 
   with_options presence: true do
     validates :item
     validates :description
+    validates :price
+  end
+
+  with_options presence: true, numericality: { other_than: 1 } do
     validates :category_id
     validates :condition_id
     validates :post_cost_id
     validates :sender_id
     validates :ship_date_id
-    validates :price
   end
 
 
