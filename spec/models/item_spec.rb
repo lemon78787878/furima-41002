1b6dc3d7c397c2bb46f3ad9023ba7964f269d1c9
lemon78787878 <wakuwaku.5611@gmail.com>
2024-07-01 14:29:50 +0900

    指摘部分の修正

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -99,6 +99,16 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include("Price must be less than or equal to 9999999")
       end
+      it 'priceが半角数値ではない場合保存できない' do
+        @item.price = "１０００"
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is not a number")
+      end
+      it 'priceが文字列では保存できないこと' do
+        @item.price = 'abc'
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Price is not a number')
+      end
 
 
 
