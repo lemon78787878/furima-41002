cecc26e7201f5d6ec871c96a63b1ea9f7752c894
lemon78787878 <wakuwaku.5611@gmail.com>
2024-07-06 01:26:48 +0900

    コントローラの再設定

--- app/controllers/buys_controller.rb
+++ app/controllers/buys_controller.rb
@@ -1,15 +1,16 @@
 class BuysController < ApplicationController
   before_action :authenticate_user!, only: [:index, :create]
-  before_action :set_item, only: [:index, :create]
   before_action :check_if_sold, only: [:index, :create]
   before_action :check_owner, only: [:index, :create]
 
   def index
+    @item = Item.find(params[:item_id])
     gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
     @buy_address = BuyAddress.new
   end
 
   def create
+    @item = Item.find(params[:item_id])
     @buy_address = BuyAddress.new(buy_params)
     if @buy_address.valid?
       pay_item
@@ -27,9 +28,6 @@ class BuysController < ApplicationController
     params.require(:buy_address).permit(:post_code, :sender_id, :city, :street, :building, :telephone).merge(user_id: current_user.id, item_id: @item.id, token: params[:token])
   end
 
-  def set_item
-    @item = Item.find(params[:id])
-  end
 
   def pay_item
     Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
@@ -41,12 +39,14 @@ class BuysController < ApplicationController
   end
 
   def check_if_sold
+    @item = Item.find(params[:item_id])
     if @item.buy.present?
       redirect_to root_path
     end
   end
 
   def check_owner
+    @item = Item.find(params[:item_id])
     if @item.user == current_user
       redirect_to root_path
     end
